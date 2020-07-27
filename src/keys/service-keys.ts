25358b03bf58ac747f791b5b5cce4762f443ad1d
Jeferson Arango <jeferson.arango@ucaldas.edu.co>
2020-07-27 17:55:31 -0500

    service keys update

--- src/keys/service-keys.ts
+++ src/keys/service-keys.ts
@@ -5,5 +5,5 @@ export namespace ServiceKeys {
   export const AES_SECRET_KEY = 'AES@SecretKey*';
   export const LOGIN_CRYPT_METHOD = MD5;
   export const JWT_SECRET_KEY = 'JWT@SecretKey*';
-  export const TOKEN_EXPIRATION_TIME = Math.floor(Date.now() / 1000) * 3600;
+  export const TOKEN_EXPIRATION_TIME = Math.floor(Date.now() / 1000) * 3600 * 3;
 }
